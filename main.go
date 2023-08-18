166d164bee2ceb90f7b1d76b134754b1e70ea5a6
dirkarnez <smalldirkalex@gmail.com>
2023-08-18 20:46:57 +0800

    - update main.go

--- main.go
+++ main.go
@@ -2,37 +2,31 @@ package main
 
 import (
 	"fmt"
-	"golang.org/x/exp/ebnf"
 	"strings"
+
+	"golang.org/x/exp/ebnf"
 )
 
 func main() {
-	// Define the grammar for the arithmetic expressions DSL
-	grammar := `
-		expr     = term { "+" term } .
-		term     = factor { "*" factor } .
-		factor   = number | "(" expr ")" .
-		number   = digit { digit } .
-		digit    = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" .
-	`
 
+	grammar := `
+		Program = Start .
+		Start = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" .
+	`
 	// Parse the grammar
-	parser, err := ebnf.Parse("arithmetic", grammar)
+	parser, err := ebnf.Parse("", strings.NewReader(grammar))
 	if err != nil {
 		fmt.Println("Error parsing grammar:", err)
 		return
 	}
-
+	fmt.Println("parser:", parser)
 	// Input expression to parse
-	expression := "2 + (3 * 4)"
+	expression := `Program`
 
 	// Parse the expression using the generated parser
-	ast, err := ebnf.ParseAST(parser, strings.NewReader(expression))
+	err = ebnf.Verify(parser, expression)
 	if err != nil {
 		fmt.Println("Error parsing expression:", err)
 		return
 	}
-
-	// Print the abstract syntax tree (AST)
-	fmt.Println("AST:", ast)
 }
